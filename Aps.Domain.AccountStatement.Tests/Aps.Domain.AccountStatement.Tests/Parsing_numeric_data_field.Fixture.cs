f70dbb520926cff1cc15f6e48cabad96fa719c17
Adrian Freemantle <adrian@synerics.com>
2016-06-04 10:27:28 +0200

    added parsing numeric data test

---
+++ Aps.Domain.AccountStatement.Tests/Aps.Domain.AccountStatement.Tests/Parsing_numeric_data_field.Fixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LightBDD;
+
+namespace Aps.Domain.AccountStatement.Tests
+{
+    public partial class Parsing_numeric_data_field : FeatureFixture
+    {
+        private void Then_a_valid_decimal_value_is_returned()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void When_parsing_the_data_pair()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Given_a_data_pair_that_maps_to_a_numeric_value()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Aps.Domain.AccountStatement.Tests/Aps.Domain.AccountStatement.Tests/Parsing_numeric_data_field.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LightBDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Aps.Domain.AccountStatement.Tests
+{
+    [TestClass]
+    [Label("Parser-1")]
+    [FeatureDescription(@"Parse text data to numerical representation so that integrity checks can be performed")]
+    public partial class Parsing_numeric_data_field
+    {
+        [TestMethod]
+        public void Parsing_a_valid_numeric_value_from_a_data_pair()
+        {
+            Runner.RunScenario(
+                Given_a_data_pair_that_maps_to_a_numeric_value,
+                When_parsing_the_data_pair,
+                Then_a_valid_decimal_value_is_returned);
+        }
+    }
+}
