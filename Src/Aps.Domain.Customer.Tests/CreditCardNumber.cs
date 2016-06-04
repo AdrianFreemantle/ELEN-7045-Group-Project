76a00a7844fac732a7f335c9eace8acc7ace5dc5
Adrian Freemantle <adrian@synerics.com>
2016-06-04 16:48:39 +0200

    completed initial scaffolding and test examples

--- Src/Aps.Domain.Customer.Tests/CreditCardNumber.cs
+++ Src/Aps.Domain.Customer.Tests/CreditCardNumber.cs
@@ -2,14 +2,9 @@
 
 namespace Aps.Domain.Customer.Tests
 {
-    public struct CreditCardNumber : IEquatable<CreditCardNumber>
+    public struct CreditCardNumber 
     {
         public CreditCardNumber(string cardNumber) : this()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Equals(CreditCardNumber other)
         {
             throw new NotImplementedException();
         }
