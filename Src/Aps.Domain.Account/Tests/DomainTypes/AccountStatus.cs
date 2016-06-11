8bcd90ecf8e007ffd106e3adef2280d69d3b2baf
Chris Viljoen <chrisv@live.co.za>
2016-06-11 16:02:51 +0200

    Updated AccountStatus Domain Type to Enums

--- Src/Aps.Domain.Account/Tests/DomainTypes/AccountStatus.cs
+++ Src/Aps.Domain.Account/Tests/DomainTypes/AccountStatus.cs
@@ -1,6 +1,49 @@
+using System;
+
 namespace Aps.Domain.Account.Tests.Tests.DomainTypes
 {
-    internal class AccountStatus
+    public class StatusNotificationRuleAttribute : Attribute
     {
+        public bool ShouldNotifyCustomer { get; set; }
+    }
+
+    public struct AccountStatus
+    {
+        private enum AccountStatusType
+        {
+            [System.ComponentModel.Description("Unknown Account Status")]
+            Unknown,
+            [StatusNotificationRule(ShouldNotifyCustomer = true)]
+            Active,
+            Inactive,
+            ThisIsComplex
+        }
+
+        private readonly AccountStatusType accountStatus;
+
+        public static AccountStatus Idiot { get { return new AccountStatus(AccountStatusType.Unknown); } }
+        public static AccountStatus Active { get { return new AccountStatus(AccountStatusType.Active); } }
+        public static AccountStatus Inactive { get { return new AccountStatus(AccountStatusType.Inactive); } }
+        public static AccountStatus ThisIsComplex { get { return new AccountStatus(AccountStatusType.ThisIsComplex); } }
+
+        private AccountStatus(AccountStatusType accountStatus)
+        {
+            this.accountStatus = accountStatus;
+        }
+
+        public bool ShouldNotifyCustomer()
+        {
+            var attribute = accountStatus.TryGetCustomAttribute<StatusNotificationRuleAttribute>();
+
+            if (attribute == null)
+                return false;
+
+            return attribute.ShouldNotifyCustomer;
+        }
+
+        public override string ToString()
+        {
+            return accountStatus.GetDescription();
+        }
     }
 }
