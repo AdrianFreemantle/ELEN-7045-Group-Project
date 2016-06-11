8658ceb52b639a44fe885045a686a4a8f3e908be
Chris Viljoen <chrisv@live.co.za>
2016-06-11 13:57:18 +0200

    Added initial Domain Types for Account Tests

---
+++ Src/Aps.Domain.Account/Tests/DomainTypes/Account.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using Aps.Domain.Account;
+
+namespace Aps.Domain.Account.Tests.Tests.DomainTypes
+{
+    public struct Account
+    {
+        private readonly AccountId accountId;
+        private readonly ICustomerId customerId;
+        private readonly AccountStatus accountStatus;
+        private readonly List<Credential> credentials;
+
+        private Account(ICustomerId customerId, AccountId accountId)
+        {
+            this.accountId = accountId;
+            this.customerId = customerId;
+            this.accountStatus = new AccountStatus();
+            this.credentials = new List<Credential>();
+        }
+
+        public static Account Create<TCustomerId>(TCustomerId customerId, AccountId accountId) where TCustomerId : struct, ICustomerId
+        {
+            Guard.ThatParameterNotDefaut(customerId, "CustomerId");
+            Guard.ThatParameterNotDefaut(accountId, "AccountId");
+
+            return new Account(customerId, accountId);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1}", accountId, customerId);
+        }
+    }
+}
