77425e8435dd4f84e80f8c07c89f43343946f959
Chris Viljoen <chrisv@live.co.za>
2016-06-11 16:03:06 +0200

    Added CustomerIdStub

---
+++ Src/Aps.Domain.Account/Tests/Stubs/CustomerIdStub.cs
@@ -0,0 +1,17 @@
+namespace Aps.Domain.Account.Tests.Tests.Stubs
+{
+    public struct CustomerIdStub : ICustomerId
+    {
+        private readonly string customerId;
+
+        public CustomerIdStub(string customerId)
+        {
+            this.customerId = customerId;
+        }
+
+        public override string ToString()
+        {
+            return customerId;
+        }
+    }
+}
