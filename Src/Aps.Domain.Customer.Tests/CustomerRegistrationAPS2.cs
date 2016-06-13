fc572645b5436cac0e2b37f8b94f537e1da1aa60
Leslie <lesliedob@gmail.com>
2016-06-13 12:40:30 +0200

    Added Tests for APS Login

---
+++ Src/Aps.Domain.Customer.Tests/CustomerRegistrationAPS2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace Aps.Domain.Customer.Tests
+{
+    partial class CustomerRegistration
+    {
+
+        private Exception thrownError;
+        private String custName;
+        private String custSurname;
+        private String custEmail;
+
+
+        private void that_a_customer_enters_a_name(string custName)
+        {
+
+           
+
+            if (custName.Length < 1)
+            {
+                throw new NotImplementedException();
+            }
+           
+        }
+
+        private void a_customer_enters_a_surname(string custSurname)
+        {
+
+            if (custSurname.Length < 1)
+            {
+                throw new NotImplementedException();
+            }
+          
+        }
+
+        private Boolean a_customer_enters_email_address(string custEmail)
+        {
+            string expresion;
+            expresion = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+
+            if (Regex.IsMatch(custEmail, expresion))
+            {
+
+
+                Console.WriteLine("Inside  the email method");
+
+                if (Regex.Replace(custEmail, expresion, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                 //   return false;
+                    throw new Exception("Invalid email adress");
+                }
+            }
+            else
+            {
+              //  return false;
+                throw new Exception("Invalid email adress");
+            }
+
+        }
+
+        private void the_users_pushes_the_register_button()
+        {
+          //  throw new NotImplementedException();
+        }
+
+        private void a_email_is_sent_to_confirm_the_registration()
+        {
+         //   throw new NotImplementedException();
+        }
+    }
+}
