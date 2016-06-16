2052a9bc443b53bbe4004b5505f81c6bcdedc730
peter cousins <peter.cousins@standardbank.co.za>
2016-06-16 20:39:27 +0200

    Added new files

---
+++ Src/Aps.Domain.Scrap.Tests/Fixtures/ScrapResultDataPair_Data_Collection.Fixture.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aps.Domain.Common;
+using Aps.Domain.Scrap.Tests.DomainTypes;
+using LightBDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Aps.Domain.Scrap.Tests
+{
+    public partial class ScrapResultDataPair_Data_Collection : FeatureFixture
+    {
+
+        private ScrapeResultDataPair dataPair;
+        private Exception error;
+        private string fieldName;
+        private string fieldValue;
+        private string id;
+
+        void an_id(string id)
+        {
+            this.id = id;
+        }
+
+        void a_field_name(string fieldName)
+        {
+            this.fieldName = fieldName;
+        }
+
+        void a_field_value(string fieldValue)
+        {
+            this.fieldValue = fieldValue;
+        }
+
+        void creating_data_pair()
+        {
+            try
+            {
+                dataPair = new ScrapeResultDataPair(id, fieldName, fieldValue);
+            }
+            catch (Exception ex)
+            {
+                error = ex;
+            }
+        }
+
+        void exception_thrown()
+        {
+            Assert.IsNotNull(error);
+        }
+    }
+}
