666d4c17594a215a3f7398ab1cbaf985e9daae19
Lishen Ramsudh <lishen.ramsudh@gmail.com>
2016-06-16 18:23:23 +0200

    Added fixture and tests for the ComapnyName value object

---
+++ Src/Aps.Domain.Company.Tests/Company_Identity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using LightBDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aps.Domain.Company.Tests
+{
+    [TestClass]
+    [FeatureDescription(@"A company must be uniquely identifiable")]
+    public partial class Company_Identity
+    {
+        [TestMethod]
+        public void Two_companies_with_the_same_name_are_equal()
+        {
+            var first = new CompanyName("Company1");
+            var second = new CompanyName("Company1");
+            
+            Runner.RunScenario(
+                given => a_company_name(first),
+                and => another_company_name(second),
+                when => performing_an_equality_comparison(),
+                then => the_two_are_equal());
+        }
+
+        [TestMethod]
+        public void Two_companies_with_different_names_are_not_equal()
+        {
+            
+        }
+    }
+
+    public struct CompanyName : IEquatable<CompanyName>
+    {
+        public string Name { get; private set; }
+
+        public CompanyName(string name)
+        {
+            this.Name = name;
+        }
+
+        public bool Equals(CompanyName other)
+        {
+            return Name.Equals(other.Name);
+        }
+    }
+}
---
+++ Src/Aps.Domain.Company.Tests/Fixtures/Company_Identity.Fixture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LightBDD;
+using Shouldly;
+
+namespace Aps.Domain.Company.Tests
+{
+    public partial class Company_Identity : FeatureFixture
+    {
+        private CompanyName companyName1;
+        private CompanyName companyName2;
+        private bool areEqual;
+
+        private void a_company_name(CompanyName first)
+        {
+            companyName1 = first;
+        }
+
+        private void another_company_name(CompanyName second)
+        {
+            companyName2 = second;
+        }
+
+        private void performing_an_equality_comparison()
+        {
+            areEqual = companyName1.Equals(companyName2);
+        }
+
+        private void the_two_are_equal()
+        {
+            areEqual.ShouldBe(true);
+        }
+    }
+}
