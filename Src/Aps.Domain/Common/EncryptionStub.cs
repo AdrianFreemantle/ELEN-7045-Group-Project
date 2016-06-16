9a4c7c06b767262d8262aa5012e1be148a94a357
Chris Viljoen <chrisv@live.co.za>
2016-06-16 16:14:50 +0200

    Added Encryption Service

---
+++ Src/Aps.Domain/Common/EncryptionStub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aps.Domain.Common
+{
+    public class EncryptionStub : IDecryptionService, IEncryptionService
+    {
+        public string Decrypt(byte[] data)
+
+        {
+            return String.Empty;
+        }
+        
+        public byte[] Encrypt(string data)
+        {
+            return new byte[0];
+        }
+    }
+}
