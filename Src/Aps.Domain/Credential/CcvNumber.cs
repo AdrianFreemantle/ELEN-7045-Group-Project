019a4a6534ffbc5fbed6fa7d7fca561b7f2f8d6b
Leslie <lesliedob@gmail.com>
2016-06-17 22:57:16 +0200

    reset

---
+++ Src/Aps.Domain/Credential/CcvNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aps.Domain.Credential
+{
+    public struct CcvNumber : IIdentificationField
+    {
+
+        private readonly string _ccvnumber;
+
+        public CcvNumber(string ccvnumber)
+        {
+            Guard.ThatParameterNotNullOrEmpty(ccvnumber , "Ccv Number");
+
+            if (!Validator.CcvIsValid(ccvnumber))
+            {
+                throw new DomainException("Ccv Number", "Invalid Ccv Number passed");
+            }
+
+            this._ccvnumber = ccvnumber;
+
+        }
+
+        public override string ToString()
+        {
+            return _ccvnumber;
+        }
+
+    }
+}
---
+++ Src/Aps.Domain/Credential/NameVO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aps.Domain.Credential
+{
+    public struct NameVO : IIdentificationField
+    {
+
+        private readonly string _name;
+
+        public NameVO(string namevo)
+        {
+            Guard.ThatParameterNotNullOrEmpty(namevo, "First Name");
+
+            if (!Validator.NameVoIsValid(namevo))
+            {
+                throw new DomainException("First name credential", "First Name Invalid");
+            }
+
+            this._name = namevo; 
+        }
+
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+}
