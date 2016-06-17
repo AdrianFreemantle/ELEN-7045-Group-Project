3b874288b72da1598504a8cfb76f0ab36e4e5ccb
peter cousins <peter.cousins@standardbank.co.za>
2016-06-17 20:38:47 +0200

    Commit the new scrapper files

---
+++ Src/Aps.Domain.Scrap.Tests/ScrapeRequestID_Identity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Aps.Domain.Scrap.Tests.DomainTypes;
+using LightBDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aps.Domain.Scrap.Tests
+{
+    [TestClass]
+    [FeatureDescription(@"A Scrapper session ID must be uniquely identifiable")]
+    public partial class ScrapeRequestID_Identity
+    {
+        [TestMethod]
+        public void Two_ScrapSession_with_the_same_GUI_are_equal()
+        {
+            //Arrange
+
+            Guid id = Guid.NewGuid();
+
+            var SessionID1 = new ScrapeRequestId(id);
+            var SessionID2 = new ScrapeRequestId(id);
+
+            //Act and Assert
+
+            Runner.RunScenario(
+                given => a_scrapperSessionid(SessionID1),
+                and => another_scrapperSessionid(SessionID2),
+                when => performing_an_equality_comparision(),
+                then => the_sessionID_are_equal());
+
+        }
+
+        [TestMethod]
+        public void Two_ScrapSession_with_the_different_GUI_are_not_equal()
+        {
+            //Arrange
+            Guid id = Guid.NewGuid();
+            Guid id1 = Guid.NewGuid();
+
+            var SessionID1 = new ScrapeRequestId(id);
+            var SessionID2 = new ScrapeRequestId(id1);
+
+            //Act and Assert
+
+            Runner.RunScenario(
+                given => a_scrapperSessionid(SessionID1),
+                and => another_scrapperSessionid(SessionID2),
+                when => performing_an_equality_comparision(),
+                then => the_sessionID_are_not_equal());
+
+
+        }
+    }
+}
---
+++ Src/Aps.Domain.Scrap.Tests/ScrapeResultCodeTest.cs
@@ -0,0 +1,29 @@
+﻿using LightBDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aps.Domain.Scrap.Tests
+{
+    [TestClass]
+    [FeatureDescription(@"A scrape Result Test")]
+    public partial class ScrapeResultCodeTest : FeatureFixture
+    {
+        [TestMethod]
+        public void Start_test()
+        {
+
+            //Arrange
+
+
+
+            //Act 
+
+
+            //Assert
+
+            
+
+
+        }
+    }
+    
+}
