e6fe8827322aadd89a2ae1e54319ef48df66fd33
peter cousins <peter.cousins@standardbank.co.za>
2016-06-18 18:30:56 +0200

    Added new files

---
+++ Src/Aps.Domain.Scrap.Tests/Fixtures/ScrapeRequestRepo.Fixture.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Aps.Domain.AccountStatements.Tests.Stubs;
+using Aps.Domain.Scrap.Tests.Stubs;
+using Aps.Domain.Common;
+using Aps.Domain.Scrap.Tests.DomainTypes;
+using Aps.Domain.Scraping;
+using LightBDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+// ReSharper disable once CheckNamespace
+namespace Aps.Domain.Scrap.Tests
+{
+    public partial class ScrapeRequestRepo : FeatureFixture
+    {
+
+        private AccountIdStub accountId;
+        private ScrapeResultStub scrapeSessionResult;
+        private ScrapeRequestFactory scrapeRequestFactory;
+        private ScrapeRequest scrapeRequest;
+        private ScrapeRequestId scrapeRequestId;
+        private Exception error;
+
+        private ScrapRequestRepositoryStub repository;
+        public ICompanyName Edgars { get; private set; }
+
+
+        private void a_valid_scrape_request()
+        {
+            ScrapeRequest_Id();
+            account_id();
+            scrapeRequest = scrapeRequestFactory.CreateScrapRequest(scrapeRequestId, accountId);
+        }
+
+        private void saving_the_scrap_request_data()
+        {
+            try
+            {
+                repository.Save(scrapeRequest);
+            }
+            catch (Exception e)
+            {
+                error = e;
+            }
+        }
+
+        private void then_saved()
+        {
+            repository.saved.ShouldBe(true);
+        }
+
+        private void ScrapeRequest_Id()
+        {
+            scrapeRequestId = new ScrapeRequestId(Guid.NewGuid());
+        }
+
+        private void account_id()
+        {
+
+            accountId = new AccountIdStub("112345", Edgars);
+        }
+
+        private ScrapRequestRepositoryStub an_scrap_request_repository()
+        {
+            return repository = new ScrapRequestRepositoryStub();
+        }
+    }
+}
