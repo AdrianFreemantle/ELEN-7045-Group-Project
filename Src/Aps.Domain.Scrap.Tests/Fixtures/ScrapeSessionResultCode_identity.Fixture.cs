59aba2e0e1c6cf592d5621621ac2a29fed364306
peter cousins <peter.cousins@standardbank.co.za>
2016-06-18 13:30:35 +0200

    Numerous tests focused on the key elements of the Scrapper

---
+++ Src/Aps.Domain.Scrap.Tests/Fixtures/ScrapeSessionResultCode_identity.Fixture.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aps.Domain.Scrap.Tests.DomainTypes;
+using LightBDD;
+using Shouldly;
+
+// ReSharper disable once CheckNamespace
+namespace Aps.Domain.Scrap.Tests
+{
+    public partial class ScrapeSessionResultCode_identity : FeatureFixture
+    {
+        private ScrapeSessionResultCode status1;
+        private ScrapeSessionResultCode status2;
+        private bool areEqual;
+        
+        private void the_two_are_equal()
+        {
+            areEqual.ShouldBe(true);
+        }
+
+        private void performing_an_equality_comparison()
+        {
+            areEqual = status1.Equals(status2);
+        }
+
+        private void another_ScrapeSessionResultCode_status(ScrapeSessionResultCode status)
+        {
+            status2 = status;
+        }
+
+        private void ScrapeSessionResultCode_status(ScrapeSessionResultCode status)
+        {
+            status1 = status;
+        }
+
+        private void the_two_are_not_equal()
+        {
+            areEqual.ShouldBe(false);
+        }
+    }
+}
---
+++ Src/Aps.Domain.Scrap.Tests/Stubs/ScrapeSessionResultCode_identity.cs
@@ -0,0 +1,39 @@
+﻿using Aps.Domain.Scrap;
+using Aps.Domain.Scrap.Tests.DomainTypes;
+using Aps.Domain.Scrap.Tests.Stubs;
+using LightBDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aps.Domain.Scrap.Tests
+{
+    [TestClass]
+    [FeatureDescription(@"Equality checks and In-equality checks for Scraper Status")]
+    public partial class ScrapeSessionResultCode_identity : FeatureFixture
+    {
+        [TestMethod]
+        public void Two_account_status_identities_with_the_same_status_are_equal()
+        {
+            var first = ScrapeSessionResultCode.Complete;
+            var second = ScrapeSessionResultCode.Complete;
+
+            Runner.RunScenario(
+            given => ScrapeSessionResultCode_status(first),
+            and => another_ScrapeSessionResultCode_status(second),
+            when => performing_an_equality_comparison(),
+            then => the_two_are_equal());
+        }
+
+        [TestMethod]
+        public void Two_ScrapeSessionResultCode_status_identities_with_different_statuses_are_not_equal()
+        {
+            var first = ScrapeSessionResultCode.Pending;
+            var second = ScrapeSessionResultCode.Complete;
+
+            Runner.RunScenario(
+            given => ScrapeSessionResultCode_status(first),
+            and => another_ScrapeSessionResultCode_status(second),
+            when => performing_an_equality_comparison(),
+            then => the_two_are_not_equal());
+        }
+    }
+}
