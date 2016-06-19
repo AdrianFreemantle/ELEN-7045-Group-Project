73fe76bc70eb5f9613e29b8f3d717000b18fbe62
cvil26 <chrisv@live.co.za>
2016-06-19 19:01:28 +0200

    Credential Updates
    Added the AccountNumber Credential

---
+++ Src/Aps.Domain/Credential/AccountNumber.cs
@@ -0,0 +1,15 @@
+namespace Aps.Domain.Credential
+{
+    public struct AccountNumber
+    {
+        private readonly string accountnumber;
+
+        public static AccountNumber Empty { get { return new AccountNumber(); } }
+
+        public AccountNumber(string accountnumber)
+        {
+
+            this.accountnumber = accountnumber;
+        }
+    }
+}
