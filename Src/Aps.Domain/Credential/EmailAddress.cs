9722dd1cde06aa1c27cbf3528706da302f3ec2b5
cvil26 <chrisv@live.co.za>
2016-06-19 19:00:48 +0200

    Credential Updates
    Moved Credential Validation into credential structs as its better DDD
    practice.
    Added Encryption to DateOfBirth Credential
    Added Encryption to CreditCardCredential Credential

--- Src/Aps.Domain/Credential/EmailAddress.cs
+++ Src/Aps.Domain/Credential/EmailAddress.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using System;
 
 namespace Aps.Domain.Credential
@@ -6,15 +7,31 @@ namespace Aps.Domain.Credential
     {
         private readonly string _address;
 
-        public EmailAddress(string address)
+        public EmailAddress(string address): this()
         {
             Guard.ThatParameterNotNullOrEmpty(address, "Email Address"); 
-            if (!Validator.EmailIsValid(address))
+            if (!EmailIsValid(address))
             {
                 throw new DomainException("Email Credential", "Invalid Email Address passed");
             }
 
             this._address = address;
+        }
+
+        private Regex CreateValidEmailRegex()
+        {
+            string validEmailPattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+
+            return new Regex(validEmailPattern, RegexOptions.IgnoreCase);
+        }
+        private bool EmailIsValid(string emailAddress)
+        {
+            Regex ValidEmailRegex = CreateValidEmailRegex();
+            bool isValid = ValidEmailRegex.IsMatch(emailAddress);
+
+            return isValid;
         }
 
         public override string ToString()
