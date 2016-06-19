9a6600398610ebede39cc3de82fb5ac9dab85fc3
Adrian Freemantle <adrian@synerics.com>
2016-06-19 16:13:16 +0200

    removed redundant tests

--- Src/Aps.Domain.AccountStatement.Tests/Account_statement_integrity_checks.cs
+++ Src/Aps.Domain.AccountStatement.Tests/Account_statement_integrity_checks.cs
@@ -1,11 +1,5 @@
-using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using Aps.Domain.AccountStatements.DataIntegrityChecks;
 using Aps.Domain.AccountStatements.Tests.Stubs;
 using Aps.Domain.Common;
-using Aps.Domain.Company;
-using Aps.Domain.Scraping;
 using LightBDD;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -17,7 +11,7 @@ namespace Aps.Domain.AccountStatements.Tests
     public partial class Account_statement_integrity_checks
     {
         [TestMethod]
-        public void Addition_checking()
+        public void Addition_checking_with_valid_data()
         {
             Runner.RunScenario(
                 given => a_successfull_scrape_session_for_account_id(new AccountIdStub("12345")),
@@ -35,6 +29,27 @@ namespace Aps.Domain.AccountStatements.Tests
                 and => a_scrape_result_data_pair_with_id_and_description_and_value("006", "Total Due", "R 160.00"),
                 when => creating_an_account_statment(),
                 then => Integrity_check_passed());
+        }
+
+        [TestMethod]
+        public void Addition_checking_with_invalid_data()
+        {
+            Runner.RunScenario(
+                given => a_successfull_scrape_session_for_account_id(new AccountIdStub("12345")),
+                and => a_mapping_code_id_for_entryType("001", AccountStatmentEntryType.OpeningBalance),
+                and => a_mapping_code_id_for_entryType("002", AccountStatmentEntryType.PaymentReceived),
+                and => a_mapping_code_id_for_entryType("003", AccountStatmentEntryType.NewCharges),
+                and => a_mapping_code_id_for_entryType("004", AccountStatmentEntryType.Discount),
+                and => a_mapping_code_id_for_entryType("005", AccountStatmentEntryType.Deductions),
+                and => a_mapping_code_id_for_entryType("006", AccountStatmentEntryType.TotalDue),
+                and => a_scrape_result_data_pair_with_id_and_description_and_value("001", "Opening Balance", "R 9000000.00"),
+                and => a_scrape_result_data_pair_with_id_and_description_and_value("002", "Payment Received", "R 500.00"),
+                and => a_scrape_result_data_pair_with_id_and_description_and_value("003", "New Charges", "R 200.00"),
+                and => a_scrape_result_data_pair_with_id_and_description_and_value("004", "Discount", "R 20.00"),
+                and => a_scrape_result_data_pair_with_id_and_description_and_value("005", "Deductions", "R 20.00"),
+                and => a_scrape_result_data_pair_with_id_and_description_and_value("006", "Total Due", "R 160.00"),
+                when => creating_an_account_statment(),
+                then => Integrity_check_failed());
         }
     }
 }
