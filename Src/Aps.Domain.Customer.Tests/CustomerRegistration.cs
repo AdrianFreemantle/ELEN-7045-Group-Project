667cdb40049eaac470ed85bd8cb35d2532e2a61a
Leslie <lesliedob@gmail.com>
2016-06-19 10:59:33 +0200

    Customer Registration Service

---
+++ Src/Aps.Domain.Customer.Tests/CustomerRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LightBDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Aps.Domain.Customer.Tests
+{
+    [TestClass]
+    [Label("Customer Registration")]
+    [FeatureDescription(@"Customer attemps to register")]
+    public partial class CustomerRegistration : FeatureFixture
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { 
+            Runner.RunScenario(
+                Given => that_a_customer_enters_email("man@moon.co.za"));
+        }
+
+      
+    }
+}
---
+++ Src/Aps.Domain.Customer.Tests/Fixtures/CustomerRegistration.Fixture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aps.Domain.Credential;
+using LightBDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Aps.Domain.Customer.Tests
+{
+    public partial class CustomerRegistration
+    {
+
+    }
+}
