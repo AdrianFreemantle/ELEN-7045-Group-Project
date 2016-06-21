e288d7fd4e31e88515fe0d25329b72b418b9a5cb
Lishen Ramsudh <lishen.ramsudh@gmail.com>
2016-06-21 10:26:43 +0200

    Added some nilling cycle fields

--- Src/Aps.Domain.Company.Tests/DomainTypes/BillingCycle.cs
+++ Src/Aps.Domain.Company.Tests/DomainTypes/BillingCycle.cs
@@ -14,6 +14,7 @@ namespace Aps.Domain.Company.Tests.DomainTypes
 
         public BillingCycle(LeadTime leadTime, NumberOfDaysPerCycle numberOfDaysPerCycle, RetryInterval retryInterval)
         {
+            Guard.ThatValueTypeNotDefaut(leadTime, "leadTime");
             _leadTime = leadTime;
             _numberOfDaysPerCycle = numberOfDaysPerCycle;
             _retryInterval = retryInterval;
