49d6a343d586cbb1d8e5074de4aa967495db3246
Lishen Ramsudh <lishen.ramsudh@gmail.com>
2016-06-23 18:26:41 +0200

    Added in base url equality tests

--- Src/Aps.Domain.Company.Tests/BaseUrl_Identity.cs
+++ Src/Aps.Domain.Company.Tests/BaseUrl_Identity.cs
@@ -1,14 +1,40 @@
 ﻿using System;
+using Aps.Domain.Company.Tests.DomainTypes;
+using LightBDD;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Aps.Domain.Company.Tests
 {
     [TestClass]
-    public class UnitTest1
+    [FeatureDescription(@"A base url with the same uri are equal")]
+    public partial class BaseUrl_Identity
     {
         [TestMethod]
-        public void TestMethod1()
+        public void a_base_url_with_the_same_uri_are_equal()
         {
+            var first = new BaseUrl("http://www.validuri.com");
+            var second = new BaseUrl("http://www.validuri.com");
+
+            Runner.RunScenario(
+                given => a_base_url(first),
+                and => another_base_url(second),
+                when => performing_an_equality_comparison(),
+                then => the_two_are_equal()
+                );
+        }
+
+        [TestMethod]
+        public void a_base_url_with_different_uri_are_not_equal()
+        {
+            var first = new BaseUrl("http://www.validuri.com");
+            var second = new BaseUrl("http://www.otheruri.com");
+
+            Runner.RunScenario(
+                given => a_base_url(first),
+                and => another_base_url(second),
+                when => performing_an_equality_comparison(),
+                then => the_two_are__not_equal()
+                );
         }
     }
 }
---
+++ Src/Aps.Domain.Company.Tests/Fixtures/BaseUrl_Identity.Fixture.cs
@@ -0,0 +1,38 @@
+﻿using Aps.Domain.Company.Tests.DomainTypes;
+using LightBDD;
+using Shouldly;
+
+namespace Aps.Domain.Company.Tests
+{
+    public partial class BaseUrl_Identity : FeatureFixture
+    {
+        private BaseUrl _baseUrl1;
+        private BaseUrl _baseUrl2;
+        private bool _areEqual;
+
+        private void a_base_url(BaseUrl first)
+        {
+            _baseUrl1 = first;
+        }
+
+        private void another_base_url(BaseUrl second)
+        {
+            _baseUrl2 = second;
+        }
+
+        private void performing_an_equality_comparison()
+        {
+            _areEqual = _baseUrl1.Equals(_baseUrl2);
+        }
+
+        private void the_two_are_equal()
+        {
+            _areEqual.ShouldBe(true);
+        }
+
+        private void the_two_are__not_equal()
+        {
+            _areEqual.ShouldBe(false);
+        }
+    }
+}
