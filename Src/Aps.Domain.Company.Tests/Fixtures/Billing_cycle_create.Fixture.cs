e2f954cfb55a35c2e308161c096b745344b05eea
Lishen Ramsudh <lishen.ramsudh@gmail.com>
2016-06-24 16:14:38 +0200

    Added some logic testing on billing cycle

--- Src/Aps.Domain.Company.Tests/Fixtures/Billing_cycle_create.Fixture.cs
+++ Src/Aps.Domain.Company.Tests/Fixtures/Billing_cycle_create.Fixture.cs
@@ -1,38 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Aps.Domain.Company.Tests.DomainTypes;
 using LightBDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Aps.Domain.Company.Tests
 {
     public partial class Billing_cycle_create : FeatureFixture
     {
+        private LeadTime _leadTime;
+        private NumberOfDaysPerCycle _numberOfDaysPerCycle;
+        private RetryInterval _retryInterval;
+        private BillingCycle _billingCycle;
+        private Exception _error;
+
         private void a_lead_time(LeadTime leadTime)
         {
-            throw new NotImplementedException();
+            _leadTime = leadTime;
         }
 
         private void a_number_of_days_per_cycle(NumberOfDaysPerCycle numberOfDaysPerCycle)
         {
-            throw new NotImplementedException();
+            _numberOfDaysPerCycle = numberOfDaysPerCycle;
         }
 
         private void a_retry_interval(RetryInterval retryInterval)
         {
-            throw new NotImplementedException();
+            _retryInterval = retryInterval;
         }
 
         private void creating_a_billing_cycle()
         {
-            throw new NotImplementedException();
+            try
+            {
+                _billingCycle = new BillingCycle(_leadTime, _numberOfDaysPerCycle, _retryInterval);
+            }
+            catch (Exception exception)
+            {
+                _error = exception;
+            }
         }
 
         private void it_must_be_valid()
         {
-            throw new NotImplementedException();
+            Assert.IsNull(_error);
         }
     }
 }
