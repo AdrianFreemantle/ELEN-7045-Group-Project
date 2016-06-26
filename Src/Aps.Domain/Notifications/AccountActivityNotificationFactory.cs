2167445c4ef74119789ff936974e379250153900
Leslie <lesliedob@gmail.com>
2016-06-26 18:26:14 +0200

    Added AccountActivityNotificationFactory

---
+++ Src/Aps.Domain/Notifications/AccountActivityNotificationFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aps.Domain.Notifications;
+using Aps.Domain.Credential;
+
+namespace Aps.Domain.Notifications
+{
+    public class AccountActivityNotificationFactory
+    {
+        public AccountActivityNotification Create(Account.Account account, NotificationType notificationType)
+        {
+            Guard.ThatValueTypeNotDefaut(account, "account");
+            Guard.ThatValueTypeNotDefaut(notificationType, "notificationType");
+
+            var id = AccountActivityNotificationId.Create();
+            return new AccountActivityNotification(id, account.GetAccountId(), account.GetCustomerId(), notificationType);
+        }
+    }
+}
