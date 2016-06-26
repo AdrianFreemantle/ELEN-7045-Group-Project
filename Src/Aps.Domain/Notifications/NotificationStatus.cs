a6d55db1d102cd9662c49f6541e56e253b0a89b6
Leslie <lesliedob@gmail.com>
2016-06-26 18:19:07 +0200

    Added NotificationType and NotificationStatus

---
+++ Src/Aps.Domain/Notifications/NotificationStatus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aps.Domain.Notifications
+{
+    public struct NotificationStatus
+    {
+        private enum NotificationStatusType
+        {
+            Unknown,
+            Unread,
+            Read,
+            Actioned
+        }
+
+        private readonly NotificationStatusType status;
+
+        public static NotificationStatus Unread { get { return new NotificationStatus(NotificationStatusType.Unread); } }
+        public static NotificationStatus Read { get { return new NotificationStatus(NotificationStatusType.Read); } }
+
+        private NotificationStatus(NotificationStatusType status)
+        {
+            this.status = status;
+        }
+    }
+}
---
+++ Src/Aps.Domain/Notifications/NotificationType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aps.Domain.Notifications
+{
+    public struct NotificationType
+    {
+
+        private enum NotificationTypeEnum
+        {
+            Unknown,
+            InvalidAccountCredentials
+        }
+
+        private readonly NotificationTypeEnum notificationType;
+
+        public static NotificationType InvalidAccountCredentials { get { return new NotificationType(NotificationTypeEnum.InvalidAccountCredentials); } }
+
+        private NotificationType(NotificationTypeEnum notificationType)
+        {
+            this.notificationType = notificationType;
+        }
+    }
+}
