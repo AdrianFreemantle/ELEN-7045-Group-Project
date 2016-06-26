ce48311b6eca562c975e8fd9977423edf96a439c
Leslie <lesliedob@gmail.com>
2016-06-26 19:00:22 +0200

    Added IAccountActivityNotificationRepository.cs

---
+++ Src/Aps.Domain/IAccountActivityNotificationRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aps.Domain.Notifications;
+
+namespace Aps.Domain
+{
+    public interface IAccountActivityNotificationRepository
+    {
+        AccountActivityNotification GetById(AccountActivityNotificationId id);
+        void Save(AccountActivityNotification accountActivityNotification);
+    }
+}
