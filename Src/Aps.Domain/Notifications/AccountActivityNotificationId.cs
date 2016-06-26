7c2909fb46380b213c51519d9c8b1f2ee7323974
Leslie <lesliedob@gmail.com>
2016-06-26 18:15:01 +0200

    Added AccountActivityNotificationId

---
+++ Src/Aps.Domain/Notifications/AccountActivityNotificationId.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aps.Domain;
+
+namespace Aps.Domain.Notifications
+{
+    public struct AccountActivityNotificationId : IIdentificationField
+    {
+        private readonly Guid id;
+
+        private AccountActivityNotificationId(Guid id)
+        {
+            this.id = id;
+        }
+
+        public override string ToString()
+        {
+            return id.ToString();
+        }
+
+        public static AccountActivityNotificationId Create()
+        {
+            return new AccountActivityNotificationId(Guid.NewGuid());
+        }
+    }
+}
